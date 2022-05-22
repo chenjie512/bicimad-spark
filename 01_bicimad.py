678e18fe44b7ac8a3378fa3c86889e708be73c0c
Chenjie <chenjieh@ucm.es>
2022-05-22 22:18:42 +0200

    correciones de los ficheros .py

--- 01_bicimad.py
+++ 01_bicimad.py
@@ -32,7 +32,7 @@ def main(sc, filename):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} <filename>")
+        print(f"Usage: python3 {sys.argv[0]} <filename.json>")
         exit(1)
     filename = sys.argv[1]
     sc = SparkContext()
--- 03_bicimad.py
+++ 03_bicimad.py
@@ -21,27 +21,39 @@ def main(sc, filename):
         .filter(lambda x: x[4] in [1, 2])\
         .map(lambda x: x[:4])
     
+    
+    
     # Extracción de tiempos de viaje por identificador de persona
     rdd_travelTime_id = rdd.map(lambda x: (x[0], x[3]))\
                         .groupByKey()\
                         .mapValues(sum)\
                         .values()
+    # Extracción del número de viajes por identificador de persona
+    rdd_travelUses_id = rdd.map(lambda x: (x[0], x[3]))\
+                        .groupByKey()\
+                        .mapValues(len)\
+                        .values()
 
-    # Tiempo medio por identificador diario (persona)
+    # Tiempo medio y uso medio por identificador diario (persona)
     total_travels_id = rdd_travelTime_id.count()
     total_travelTime_id = rdd_travelTime_id.sum()
+    total_travelUses_id = rdd_travelUses_id.sum()
+    
     average_travelTime_id = total_travelTime_id/total_travels_id
+    average_travelUses_id = total_travelUses_id/total_travels_id
     
     out = open(f'03_out.txt', 'w')
     out.write(f'Personas (id): {total_travels_id}\n')
     out.write('Duración total: ' + format(total_travelTime_id/3600, ".2f") + ' horas\n')
-    out.write('Media por persona: ' + format(average_travelTime_id/60, ".2f") + ' minutos\n')
+    out.write(f'Usos Totales: {total_travelUses_id} usos\n')
+    out.write('Tiempo de viaje medio por persona: ' + format(average_travelTime_id/60, ".2f") + ' minutos\n')
+    out.write('Media de usos por persona: '+ format(average_travelUses_id, ".2f") + ' usos por persona')
     out.close()
   
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} <filename>")
+        print(f"Usage: python3 {sys.argv[0]} <filename.json>")
         exit(1)
     filename = sys.argv[1]
     sc = SparkContext()
